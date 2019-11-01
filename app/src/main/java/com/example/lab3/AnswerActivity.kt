fe15dac3372b6878f07e72664a5c7666b0631831
Дмитрий Коптик <43681022+DmitriyKoptik@users.noreply.github.com>
2019-11-01 16:36:30 +0300

    Third time

---
+++ app/src/main/java/com/example/lab3/AnswerActivity.kt
@@ -0,0 +1,54 @@
+package com.example.lab3
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_answer.*
+import kotlinx.android.synthetic.main.fragment_activity.*
+
+class AnswerActivity : AppCompatActivity() {
+
+    private lateinit var fragment: MyFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+        textViewTwo.text = intent.getStringExtra("question")
+
+        this.fragment = supportFragmentManager.findFragmentById(R.id.second) as MyFragment
+        fragment.button.setOnClickListener {
+            val intent = Intent().apply {
+                putExtra("answer", fragment.editText.text.toString())
+            }
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.close -> {
+                val exitDialog = ExitDialog()
+                exitDialog.show(supportFragmentManager, "dialog")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun finishAndRemoveTask() {
+        val intent = Intent().apply {
+            putExtra("endgame", "endgame")
+        }
+        setResult(Activity.RESULT_OK, intent)
+        super.finishAndRemoveTask()
+    }
+}
---
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.lab3
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_activity.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragment: MyFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.fragment = supportFragmentManager.findFragmentById(R.id.first) as MyFragment
+        fragment.button.setOnClickListener {
+            val intent = Intent(this, AnswerActivity::class.java).apply {
+                putExtra("question", fragment.editText.text.toString())
+            }
+            startActivityForResult(intent, 12)
+        }
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 12) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data?.getStringExtra("endgame").equals("endgame")) {
+                    finish()
+                }
+                textViewMain.text = data?.getStringExtra("answer")
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.close -> {
+                val exitDialog = ExitDialog()
+                exitDialog.show(supportFragmentManager, "dialog")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
